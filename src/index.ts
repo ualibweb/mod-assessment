2559b0ea01b02e13a8a0900dfef6284182fc3930
jaredrcleghorn <jrcleghorn@crimson.ua.edu>
2019-06-17 22:09:40 +0000

    Add endpoint /location-units/institutions

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,21 @@ import config from './config';
 const app = express();
 const client: Client = new Client(config.pg);
 
+app.get('/assessment/location-units/institutions', (request: express.Request, response: express.Response) => {
+  client.query(`SELECT * FROM institutions`)
+    .then(result => {
+      response.json(result.rows);
+    })
+    .catch(error => {
+      response.sendStatus(500);
+      console.error(error.stack);
+    });
+});
+
 // Connect to the LDP and start listening.
 client.connect()
   .then(() => { app.listen(config.express.port, () => { console.log(`Assessment API listening on port ${config.express.port}!`); }); })
-  .catch((error: Error) => {
+  .catch(error => {
     console.error(error.stack);
     process.exit(1);
   });
